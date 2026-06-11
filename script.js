5f3e83fbaa9b71d536224e9fa18a045a0d774a5e
dharani-244 <mdharani0896@gmail.com>
2026-06-11 16:00:33 +0530

    js day4

---
+++ script.js
@@ -0,0 +1,128 @@
+/*let name="dharani";
+var age=18;
+const height=1.34;
+console.log(age+2);
+console.log(name);
+console.log(height+2);
+console.log(age-2);
+console.log(age%4);
+age+=3;
+console.log(age);
+age-=2;
+let age1=20;
+console.log(age<age1);
+console.log(age>age1);
+console.log(age<=age1);
+console.log(age>=age1);*/
+
+/*let money=4300;
+let m=0;
+let n=0;
+let o=0;
+if(money!==0){
+     m=money%500;
+    let n1=Math.floor(money/500);
+    console.log("no. of 500 notes",n1);
+}
+
+ if(m!=0){
+     n=m%200;
+        let n2=Math.floor(m/200);
+        console.log("no. of 200 notes",n2);
+}
+
+if(n!=0){
+         o=n%100;
+        let n3=Math.floor(n/100);
+        console.log("no. of 100 notes"+n3);
+    
+ }*/
+/*for(let i=1;i<10;i++){
+    console.log(i+ "days to complete course");
+}
+let i=1;
+while(i<=10){
+    console.log(i+" days");
+    i++;
+}
+for(let i=1;i<=10;i++){
+    console.log("2 x "+i+" = "+(2*i));
+}
+let i=1
+let g;
+while(i<=5){
+    for(j=1;j<5;j++){
+        console.log("last person");
+    }
+    i=i+2;
+    
+
+}
+console.log(i)*/
+
+
+/*function gum()
+{
+    console.log("horror stories in this tv");
+    console.log("it is famous");
+    console.log("in thisthere is one character who hepls people and deal with goast");
+    console.log("all are love that character");
+
+}
+for(let i=1;i<=5;i++){
+    gum();
+}
+
+function tv(characters){
+    console.log(characters+" is a character in chewing gum tv");
+}
+for(let i=1;i<3;i++){
+    tv(2);
+}
+function add(a,b)
+{
+    console.log(add+" ouput");
+}
+add(4,5);
+
+function guess(n)
+{
+    let userguess=Number(prompt("guess number"));
+    if(userguess==n){
+        console.log("congratualation! you guess right");
+    }
+    else if(userguess<n){
+        console.log("try the bigger number");
+        guess(n);
+        }
+    else{
+        console.log("try the small number");
+        guess(n);
+    }
+}
+let number=Math.floor(Math.random()*100)+1;
+guess(number);*/
+/*function num(n)
+{
+    if(n>10){
+        return;
+    
+    }
+    console.log(n);
+    n+=1;
+    num(n);
+}
+num(1);*/
+let chocolates=["melodi","kachamango","dark","silk"];
+let sales={
+    dharani:chocolates[0],
+    dharani1:chocolates[1],
+    dharani2:chocolates[3],
+    dharani3:chocolates[2]
+}
+console.log(sales);
+
+
+
+ 
+
